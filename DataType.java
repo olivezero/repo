dd0fe68f49d12bb8406f7da450f52acbf986f41f
olivezero <autumber9@gmail.com>
2022-05-18 09:37:26 +0900

    hi

---
+++ DataType.java
@@ -0,0 +1,7 @@
+public class DataType{
+	public static void main(String[] args) {
+		System.out.println(6);
+		System.out.println("six");
+		System.out.println("..zdk1".length());
+	}
+}
---
+++ Number.java
@@ -0,0 +1,11 @@
+
+public class Number{
+	
+	public static void main(String[] args) {
+		
+		int a; 
+		a = 1;
+		System.out.println(a);
+		
+	}
+}
